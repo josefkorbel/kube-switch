9886d988661a2792df1793f9786f661bafd5f10c
Josef Korbel <josef.korbel@neuronsw.com>
2019-02-28 19:28:40 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# kube-switch
+Small bash utility to switch between kubernetes clusters using a visual version of kubectl commands
---
+++ switch.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+IFS=$'\n' clusters=($(kubectl config get-contexts --no-headers | awk '{print $2}'))
+active_cluster=$(kubectl config current-context)
+
+for i in ${!clusters[@]};
+do
+    :
+    if [ "${clusters[$i]}" == $active_cluster ]; then
+        echo -e "\e[34m[*] \e[36m"${clusters[$i]}
+    else
+        echo -e "\e[34m[$((i+1))] \e[36m"${clusters[$i]}
+    fi
+done
+
+read -p "Select cluster: " cluster
+
+if [[ -z "$cluster" ]]; then
+    echo -e "\e[91mNothing selected"
+    exit 1
+fi
+
+if (( cluster - 1 < ${#clusters[@]} )) && (( cluster > 0 )); then
+    echo -e "\e[32mSelected cluster: ${cluster[$cluster - 1]}"
+    kubectl config use-context ${clusters[$cluster - 1]}
+else
+    echo -e "\e[91mOops, $cluster is not there"
+    exit 1
+fi
+
